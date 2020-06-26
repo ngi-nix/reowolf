ba995098fc25b9a52c830100e7d9c5affbaa05eb
Christopher Esterhuyse <christopher.esterhuyse@gmail.com>
2020-06-26 13:11:46 +0200

    cleaned up and updated dependencies

--- examples/make.py
+++ examples/make.py
@@ -1,15 +1,15 @@
 import os, glob, subprocess
 script_path = os.path.dirname(os.path.realpath(__file__));
 for c_file in glob.glob(script_path + "/*/*.c", recursive=False):
-	print("compiling", c_file)
-	args = [
-		"gcc", 
-		"-L",
-		"../target/release",
-		"-lreowolf_rs",
-		"-Wl,-R../target/release",
-		c_file,
-		"-o",
-		c_file[:-2]
-	];
-	subprocess.run(args);
+  print("compiling", c_file)
+  args = [
+    "gcc",          # compiler
+    "-L",           # lib path flag
+    "./",           # where to look for libs
+    "-lreowolf_rs", # add lib called "reowolf_rs"
+    "-Wl,-R./",     # pass -R flag to linker: produce relocatable object
+    c_file,         # input source file
+    "-o",           # output flag
+    c_file[:-2]     # output filename
+  ];
+  subprocess.run(args);
