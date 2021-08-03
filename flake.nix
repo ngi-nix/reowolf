65e2ad8fc865c8fb295690699440461738a6570d
ostylk <ostylk@googlemail.com>
2021-08-03 11:59:13 +0200

    Summer of Nix: package this library as a nix flake
    The nix flakes provides this rust library as a nixpkgs overlay or normal
    package attribute. A dev shell environment is also made available and
    tests are automatically executed on a CI to ensure library correctness.

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "This library builds upon the previous Reowolf 1.0 implementation, with the intention of incrementally moving towards Reowolf 2.0. The goals of Reowolf 2.0 are to provide a generalization of sockets for communication over the internet, to simplify the implementation of algorithms that require distributed consensus, and to have a clear delineation between protocol and data whenever information is transmitted over the internet.";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable-small";
+
+  outputs = { self, nixpkgs }:
+    let
+      pname = "reowolf";
+      version = "1.1.0";
+
+      supportedSystems = [ "x86_64-linux" ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
+    in
+    {
+      overlay = final: prev: {
+        reowolf = with final; rustPlatform.buildRustPackage rec {
+          inherit pname version;
+
+          src = builtins.path { path = ./.; name = pname; };
+
+          cargoPatches = [ ./add-Cargo.lock.patch ];
+
+          cargoSha256 = "sha256-PgqcsvbnyseGTpwgemnE+ZsV361DJKe+4ldP5AjMDFc=";
+
+          outputs = [ "out" "headers" ];
+
+          postFixup = ''
+            mkdir -p $headers/include
+
+            cp reowolf.h $headers/include/
+            cp pseudo_socket.h $headers/include/
+          '';
+        };
+      };
+
+      packages = forAllSystems (system:
+        {
+          inherit (nixpkgsFor.${system}) reowolf;
+        });
+
+      defaultPackage = forAllSystems (system: self.packages.${system}.reowolf);
+
+      devShell = forAllSystems (system: self.packages.${system}.reowolf);
+
+      checks = forAllSystems (system: {
+        inherit (self.packages.${system}) reowolf;
+      });
+    };
+}
